a2827c666028017e9b89ed7ff2263a588e6a1498
rcoldwell <robert@icoldwell.com>
2015-03-01 22:59:32 -0600

    pizza js update

--- views/js/main.js
+++ views/js/main.js
@@ -453,10 +453,10 @@ var resizePizzas = function(size) {
 
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
-    for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
-      var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
-      var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
-      document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
+    for (var i = 0; i < document.getElementsByClassNameName(".randomPizzaContainer").length; i++) {
+      var dx = determineDx(document.getElementsByClassNameName(".randomPizzaContainer")[i], size);
+      var newwidth = (document.getElementsByClassNameName(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
+      document.getElementsByClassNameName(".randomPizzaContainer")[i].style.width = newwidth;
     }
   }
 
@@ -505,7 +505,7 @@ function updatePositions() {
   frame++;
   window.performance.mark("mark_start_frame");
 
-  var items = document.querySelectorAll('.mover');
+  var items = document.getElementsByClassName('.mover');
   for (var i = 0; i < items.length; i++) {
     var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
